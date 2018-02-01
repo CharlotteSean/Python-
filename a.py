e9e3472daa56dca714eaccf5345d5bf25944b8d8
Charlotte <2308798518@qq.com>
2018-02-01 09:56:33 +0800

    abc

--- a.py
+++ a.py
@@ -2,11 +2,19 @@ import math
 import cmath
 import random
 import decimal
-import b
-filename = 'alice.txt'
-try:
-    with open(filename) as f_obj:
-        contents = f_obj.read()
-except FileNotFoundError:
-    msg = "Sorry, the file " + filename + " does not exist."
-    print(msg)
+import json
+from b import AnonymousSurvey
+#定义一个问题，并创建一个表示调查的AnonymousSurvey对象
+question = "What language did you first learn to speak?"
+my_survey = AnonymousSurvey(question)
+#显示问题并存储答案
+my_survey.show_question()
+print("Enter 'q' at any time to quit.\n")
+while True:
+    response = input("Language: ")
+    if response == 'q':
+        break
+    my_survey.store_response(response)
+# 显示调查结果
+print("\nThank you to everyone who participated in the survey!")
+my_survey.show_results()
